2839797ec0f8df21257798d803f758da53b96cce
Tibi <simonfi.tiberiu@gmail.com>
2025-03-01 23:33:46 +0200

    fix: add missing period to comment in runner script

--- vagrant-kubeadm-kubernetes-main/scripts/runner.sh
+++ vagrant-kubeadm-kubernetes-main/scripts/runner.sh
@@ -4,7 +4,7 @@ REPO_URL="https://github.com/TBSimonfi/kube_cluster"
 
 echo "Starting provisioning..."
 
-# Function to install GitHub Actions runner
+# Function to install GitHub Actions runner.
 function install_github_runner {
   sudo -u vagrant mkdir -p /home/vagrant/actions-runner && cd /home/vagrant/actions-runner
   sudo -u vagrant curl -o actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz
