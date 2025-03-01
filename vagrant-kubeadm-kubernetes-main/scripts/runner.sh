45a39b3bfdf4b38bfa4697438d2c17e74e162616
Tibi <simonfi.tiberiu@gmail.com>
2025-03-01 21:32:01 +0200

    add runner VM configuration and provisioning script for GitHub Actions

---
+++ vagrant-kubeadm-kubernetes-main/scripts/runner.sh
@@ -0,0 +1,18 @@
+# Environment variables
+GITHUB_TOKEN="AJRTUPSBWONY56XFOC6QWCDHXX6UQ"
+REPO_URL="https://github.com/TBSimonfi/test_repo"
+
+echo "Starting provisioning..."
+
+# Function to install GitHub Actions runner
+function install_github_runner {
+  sudo -u vagrant mkdir -p /home/vagrant/actions-runner && cd /home/vagrant/actions-runner
+  sudo -u vagrant curl -o actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz
+  sudo -u vagrant tar xzf ./actions-runner-linux-x64-2.305.0.tar.gz
+  sudo -u vagrant ./config.sh --url $REPO_URL --token $GITHUB_TOKEN --unattended --replace
+  sudo ./svc.sh install
+  sudo ./svc.sh start
+  echo "GitHub Actions runner installed and started."
+}
+
+install_github_runner
